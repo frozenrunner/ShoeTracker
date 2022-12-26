5cc6ad890a76368dbe55e5218c3c5e0edfde02b2
frozenrunner <mike.horlick@gmail.com>
2022-12-26 16:13:53 -0500

    Add in new shoes, update styling, attempt pallette

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@ import AddShoe from './components/AddShoe.vue'
 import brooksGhost from './assets/brooks-ghost-14-grey.jpg'
 import nikePegasus from './assets/nike-pegasus-39.jpg'
 import brooksLaunch from './assets/brooks-launch-9-boston.webp'
+import dsTrainer from './assets/asics-ds-trainer-26.webp'
+import metaSpeed from './assets/asics-metaspeed-sky.webp'
+import novaBlast from './assets/asics-novablast-3.webp'
 
 const showAddShoe = ref(false);
 
@@ -13,21 +16,48 @@ const shoes = ref([{
         img: brooksGhost,
         startDate: new Date("2022-01-01 00:00:00"),
         estKm: 1000,
-        currentKm: 230
+        currentKm: 230,
+        blurb: "The Ghost 14 is a dependable workhorse that has the perfect balance of long-distance cushioning and ride stability. It excels on long-distances and steady-paced runs."
       },
       {
         name: "Nike Pegasus 39",
         img: nikePegasus,
         startDate: new Date("2022-04-01 00:00:00"),
         estKm: 1500,
-        currentKm: 130
+        currentKm: 130,
+        blurb: "The Nike Air Zoom Pegasus 39 is a comfortable, mid-range daily trainer designed to be durable."
       },
       {
         name: "Brooks Launch 9",
         img: brooksLaunch,
         startDate: new Date("2022-05-01 00:00:00"),
         estKm: 800,
-        currentKm: 630
+        currentKm: 630,
+        blurb: "The Brooks Launch 9 will tick most of your boxes as a good all round running shoe."
+      },
+      {
+        name: "Asics Gel-DS Trainer 26",
+        img: dsTrainer,
+        startDate: new Date("2022-06-01 00:00:00"),
+        estKm: 1000,
+        currentKm: 330,
+        blurb: "The ASICS Gel-DS Trainer 26 is a top notch short-to-mid distance shoe geared toward speed,"
+      },
+      {
+        name: "Asics MetaSpeed Sky",
+        img: metaSpeed,
+        startDate: new Date("2022-07-01 00:00:00"),
+        estKm: 500,
+        currentKm: 100,
+        blurb: "The Asics Metaspeed Sky is a rocket. Its stiff carbon plate combined with its high toe-spring rocker results in one of the fastest-feeling super shoes that money can buy."
+      },
+      {
+        name: "Asics NovaBlast 3",
+        img: novaBlast,
+        startDate: new Date("2022-08-01 00:00:00"),
+        estKm: 1200,
+        currentKm: 400,
+        blurb: "The Novablast 3 is a very good long run shoe and just eats up miles."
       }]);
 
 function addShoe(newShoe) {
--- src/components/Shoe.vue
+++ src/components/Shoe.vue
@@ -14,10 +14,14 @@ const flipped = ref(false);
 
 <template>
     <div class="shoe" :class="{flipped: flipped}" @click="flipped = !flipped">
-        <div class="shoe-front">
-            <img class="img-shoe" :src="shoe.img"/>
+        <div class="box shoe-front">
+            <div class="shoe-inner">
+                <h2 class="content shoe-name">{{ shoe.name }}</h2>
+                <img class="shoe-img" :src="shoe.img"/>
+                <p class="content is-small shoe-blurb">{{ shoe.blurb }}</p>
+            </div>
         </div>
-        <div class="shoe-back">
+        <div class="box shoe-back">
             <h1>{{shoe.name}}</h1>
             <p><span class="txt-start-date">Start Date:</span> {{shoe.startDate.toDateString()}}</p>
             <p>{{shoe.currentKm}}<meter min="0" :max="shoe.estKm" :value="shoe.currentKm"></meter>{{shoe.estKm}}</p>
@@ -28,19 +32,63 @@ const flipped = ref(false);
 <style lang="scss" scoped>
 
 .shoe {
+    height: 400px;
+    max-width: 250px;
     position: relative;
-    .shoe-front {
-        background: #fff;
-        backface-visibility: hidden;
+
+    @media (min-width: 1024px) {
+        max-width: none;
+    }
+
+    .shoe-inner {
+        display: flex;
+        flex-flow: column nowrap;
         height: 100%;
+    }
+
+    .shoe-front {
+        backface-visibility: hidden;
+        background-color: var(--teal-1);
+        background-image: linear-gradient(0deg, var(--teal-1) 0%, var(--teal-2) 100%);
+        border: 1px solid var(--teal-3);
+        height: 100%;
+        padding: 0 .625rem .625rem;
         transform: rotateY(0deg);
         transform-style: preserve-3d;
         transition: transform var(--card-animation-speed) ease-in;
         z-index: 10;
+
+        .shoe-name {
+            border-radius: 5px;
+            font-weight: 700;
+            padding: .5rem;
+            margin: 0;
+        }
+
+        .shoe-img {
+            align-self: center;
+            margin: 0 0 1rem;
+            width: 200px;
+
+            @media (min-width: 1024px) {
+                margin: 0 0 1rem;
+                width: 400px;
+            }
+        }
+
+        .shoe-blurb {
+            background: var(--blue-2);
+            border: 1px solid var(--blue-4);
+            border-radius: 5px;
+            color: #fff;
+            flex: 1 1 auto;
+            padding: .5rem;
+        }
     }
 
     .shoe-back {
-        background: gray;
+        background-color: #f2f5fb;
+        background-image: linear-gradient(0deg, #f2f5fb 0%, #97D9E1 100%);
         backface-visibility: hidden;
         height: 100%;
         left: 0;
@@ -63,13 +111,6 @@ const flipped = ref(false);
             transform: rotateY(0deg);
             z-index: 10;
         }
-    }
-}
-.img-shoe {
-    width: 200px;
-
-    @media (min-width: 1024px) {
-        width: 400px;
     }
 }
 
